7b7063dc6c10d6ffa4c1e824fe895eee9f11ee99
Roxanne Devillers <roxannedevillers@gmail.com>
2023-01-09 21:54:49 -0500

    recipe model ongoing

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,39 @@
+class RecipesController < ApplicationController
+  before_action :set_recipe, only: %i[show update]
+
+  def index
+    @recipes = Recipe.all
+  end
+
+  def show
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user = current_user
+    if @recipe.save
+      redirect_to recipe_path(@recipe)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @recipe.update(recipe_params)
+    redirect_to recipe_path(@recipe)
+  end
+
+  private
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :description, :ingredients)
+  end
+end
