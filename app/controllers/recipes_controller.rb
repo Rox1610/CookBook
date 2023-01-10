66042e5865faabfc87508b1f5ea39115a57f496d
Roxanne Devillers <roxannedevillers@gmail.com>
2023-01-10 09:45:55 -0500

    new page style

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -34,6 +34,6 @@ class RecipesController < ApplicationController
   end
 
   def recipe_params
-    params.require(:recipe).permit(:title, :rich_body, :ingredients)
+    params.require(:recipe).permit(:title, :rich_body, :ingredients, :photo)
   end
 end
