60fd3c2aeff91eb9efb54c0b61ba6683222ae3a2
Roxanne Devillers <roxannedevillers@gmail.com>
2023-01-10 18:48:19 -0500

    about page ongoing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  get '/about', to: 'pages#about', as: 'about'
 
   resources :recipes
 end
